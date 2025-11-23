03966337986c24052b2ed761bb64b5a7986de7df
vxctorp12 <ferpolanco127@gmail.com>
2025-11-22 23:36:35 -0600

    c

--- frontend-moviles/capacitor.config.ts
+++ frontend-moviles/capacitor.config.ts
@@ -8,6 +8,19 @@ const config: CapacitorConfig = {
     androidScheme: 'http',
     cleartext: true,
     allowNavigation: ['192.168.0.102']
+  },
+  // --- ESTA ES LA SECCIÓN QUE FALTABA ---
+  plugins: {
+    StatusBar: {
+      // false = La app empieza DEBAJO de la barra (soluciona el solapamiento)
+      overlay: false,
+
+      // Color de fondo de la barra (Tu azul primario #2A67F1)
+      backgroundColor: '#2A67F1',
+
+      // 'DARK' en Capacitor significa "Iconos blancos para fondo oscuro"
+      style: 'DARK'
+    }
   }
 };
 
